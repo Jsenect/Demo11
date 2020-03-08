575ec838e5f4e6117f84e14c3c1a33ce4d9ed422
liuzhizheng <zhizhengl_gent@163.com>
2020-03-08 18:21:20 +0800

    工具类

---
+++ export_parent/export_commons/src/main/java/HelloController.java
@@ -0,0 +1,24 @@
+package cn.itcast.controller;
+
+import cn.itcast.demo.HelloService;
+import com.alibaba.dubbo.config.annotation.Reference;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @Reference //从注册中心中获取对象
+    private HelloService helloService;
+
+    @RequestMapping("/sayHello")
+    @ResponseBody
+    public String sayHello(String username){
+        String hello = helloService.sayHello(username);
+        return hello;
+
+    }
+
+}
